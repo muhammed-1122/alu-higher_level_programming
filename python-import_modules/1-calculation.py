f1a0881c10c5da7c982828341ec4596d660c00aa
muhammed-1122 <m.achuja@alustudent.com>
2025-06-21 07:00:06 -0700

    last

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,10 +4,10 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print("Addition: {} + {} = {}".format(a, b, add(a, b)))
-print("Subtraction: {} - {} = {}".format(a, b, sub(a, b)))
-print("Multiplication: {} * {} = {}".format(a, b, mul(a, b)))
-print("Division: {} / {} = {}".format(a, b, div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     pass
