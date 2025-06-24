0b941e436bbe4ce6e7039d98fe425bd5cb5a487e
muhammed-1122 <m.achuja@alustudent.com>
2025-06-23 21:36:47 -0700

    Pushing

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,6 +2,5 @@
 def print_list_integer(my_list=[]):
     if my_list is None:
         return
-    for number in my_list:
-        if isinstance(number, int):
-            print("{:d}".format(number))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
