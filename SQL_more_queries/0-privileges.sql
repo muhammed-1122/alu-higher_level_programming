0fb520b916d498182ee535708e3bc56282d866de
muhammed-1122 <m.achuja@alustudent.com>
2025-07-23 21:37:09 -0700

    Adding stuff

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,3 @@
 -- List privileges of user_0d_1 and user_0d_2
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'pass';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'pass';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
